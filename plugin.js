42c5176bb230b1607d49956119fa7ba42b8e3e75
albertosouza <alberto.souza.99@gmail.com>
2015-09-19 12:30:57 -0300

    update logic to use we-core we-event helper

--- plugin.js
+++ plugin.js
@@ -9,22 +9,23 @@ module.exports = function loadPlugin(projectPath, Plugin) {
   plugin.setConfigs({
     GA: {
       id: null,
-      enableEmberjs: true,
       env: {
         prod: true,
         dev: false,
         test: false
       }
-    },
-
-    template: {
-      helpers: {
-        'google-analytics': __dirname + '/server/template-helpers/google-analytics.js'
-      }
     }
   });
-  // plugin routes
-  // plugin.setRoutes();
+
+  plugin.events.on('we-html-body-end', function (data){
+    if (!data.we.config.GA.env[data.we.env]|| !data.we.config.GA.id) return;
+
+    data.html.text += '<script>(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){'+
+      '(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),'+
+      'm=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)'+
+      '})(window,document,\'script\',\'//www.google-analytics.com/analytics.js\',\'ga\');'+
+      'ga(\'create\', \''+data.we.config.GA.id+'\', \'auto\');ga(\'send\', \'pageview\');</script>\';';
+  });
 
   return plugin;
 };
