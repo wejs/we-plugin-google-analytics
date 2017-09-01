3a7c5d6485e97209b6f651e83414d1455fa98f92
albertosouza <alberto.souza.99@gmail.com>
2017-09-01 09:23:44 -0300

    move google analytics html insertion to one function

--- plugin.js
+++ plugin.js
@@ -1,5 +1,5 @@
 /**
- * We.js messenger plugin config
+ * We.js google analytics plugin main file
  */
 
 module.exports = function loadPlugin(projectPath, Plugin) {
@@ -17,7 +17,7 @@ module.exports = function loadPlugin(projectPath, Plugin) {
     }
   });
 
-  plugin.events.on('we-html-body-end', function (data){
+  plugin.addGoogleAnalytcsTag = function addGoogleAnalytcsTag(data) {
     if (!data.we.config.GA.env[data.we.env]|| !data.we.config.GA.id) return;
 
     data.html.text += '<script>(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){'+
@@ -25,7 +25,9 @@ module.exports = function loadPlugin(projectPath, Plugin) {
       'm=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)'+
       '})(window,document,\'script\',\'//www.google-analytics.com/analytics.js\',\'ga\');'+
       'ga(\'create\', \''+data.we.config.GA.id+'\', \'auto\');ga(\'send\', \'pageview\');</script>';
-  });
+  }
+
+  plugin.events.on('we-html-body-end', plugin.addGoogleAnalytcsTag);
 
   return plugin;
 };
